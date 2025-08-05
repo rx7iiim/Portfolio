02dd64c7504dacaba0ed1aa87a8e67385e9345a2
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-08-05 10:45:31 +0100

    mvp2 Header

--- components/Hedaer.tsx
+++ components/Hedaer.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 export default function Hedaer() {
   return (
-    <div className=" h-12 bg-gray-900 flex items-center px-6 z-10 border-b border-gray-800 shadow-md">
+    <div className="absolute top-0 left-0 overflow-hidden bg-gray-900 flex items-center justify-between space-x-2 px-4 py-4 border-b border-gray-800 rounded-t-md shadow-md">
+      {/* Traffic lights */}
       <div className="flex space-x-2">
         <div className="w-3 h-3 rounded-full bg-red-500"></div>
         <div className="w-3 h-3 rounded-full bg-yellow-500"></div>
         <div className="w-3 h-3 rounded-full bg-green-500"></div>
       </div>
-      <div className="ml-4 text-base text-gray-400 font-mono">
-        portfolio-terminal — bash
+
+      {/* Terminal title */}
+      <div className=" text-xs text-gray-400 font-mono select-none mr-2">
+        portfolio — bash
       </div>
     </div>
   );
