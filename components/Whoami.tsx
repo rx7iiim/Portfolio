e24205c5120f5b00ae3028e68f9f804278caed1c
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-08-05 10:47:45 +0100

    mvp2 Ml Who am i

--- components/Whoami.tsx
+++ components/Whoami.tsx
@@ -35,10 +35,20 @@ export default function AboutMe() {
 
   return (
     <section
-      className="min-h-screen w-full px-4 py-12 pb-28 bg-transparent flex items-center justify-center rounded-2xl shadow-xl mx-auto "
+      className="
+    h-full
+    lg:h-screen
+    w-full
+    lg:ml-36
+    sm:px-6 lg:px-12
+    py-6
+    bg-transparent
+    flex items-center justify-center 
+
+  "
       id="whoami"
     >
-      <div className=" h-3/4 py-2 flex items-center justify-center mt-20">
+      <div className=" py-2 flex items-center justify-center">
         <div className="max-w-4xl w-full mx-auto">
           <div className="flex flex-col md:flex-row gap-12 items-center">
             {/* Profile Image */}
@@ -76,7 +86,7 @@ export default function AboutMe() {
                 intuitive interfaces.
               </p>
 
-              <div className="mb-8">
+              <div className="mb-8 sm:mb-4">
                 <p className="text-gray-400 mb-2">
                   <span className="text-green-400">$</span> cat skills.txt
                 </p>
